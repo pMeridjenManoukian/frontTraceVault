9d94521e56a1d13966f85a3ba001f06fc6a2e7bb
pMeridjenManoukian <22168225+pMeridjenManoukian@users.noreply.github.com>
2025-11-27 10:11:52 +0100

    proto verification correction deployment 2

--- tracevault/src/app/nftcheck/page.tsx
+++ tracevault/src/app/nftcheck/page.tsx
@@ -3,33 +3,44 @@
 import React from 'react'
 import { useSearchParams } from 'next/navigation';
 import Nftcard from '@/components/shared/nftcard';
+import { useState } from 'react';
 
 export default function NftCheck() {
   const searchParams = useSearchParams();
-  const hash = searchParams.get('hash');
-  const isEtiquette = searchParams.get('isEtiquette') === 'true';
-  const isNft = searchParams.get('isNft') === 'true';
-
+  const hash = searchParams.get('message') ?? 'no hash';
+      const [isEtiquette, setisEtiquette] = useState(true);
+    const [isNft, setisNft] = useState(hash === "no hash" ? false : true);
   // ✅ Gérer le cas où hash est null
-  if (!hash) {
+const nohash = (
+    
+        <div>❌ Aucun hash fourni dans l'URL</div>
+      
+)
+const yeshash = (
+<Nftcard 
+                hashlocal={hash} 
+                isEtiquette={isEtiquette} 
+                isNft={isNft}
+                />
+)
+  if (hash === "nohash") {
     return (
-      <div className="NFT-titre">
-        ❌ Aucun hash fourni dans l'URL
+      <div className="nftcheck">
+        {nohash}
       </div>
     );
-  }
-
-  return (
-    <div>
-      <div className="NFT-titre">VERIFIER UN ELEMENT</div>
-      <Nftcard 
-        hashlocal={hash} 
-        isEtiquette={isEtiquette} 
-        isNft={isNft}
-      />
-    </div>
-  )
+  } else {
+      return (
+          <div className="nftcheck">
+            <div>
+            cliquez sur boutons ci dessous pour voir les differentes cartes
+            <div onClick={() => setisNft(!isNft)} className={`button ${isNft ? 'vert' : 'rouge'}`}>NFT ?</div>
+            <div onClick={() => setisEtiquette(!isEtiquette)} className={`button ${isEtiquette ? 'vert' : 'rouge'}`}>ETIQUETTE ?</div>
+            </div>
+            <div className="NFT-titre">VERIFIER UN ELEMENT</div>
+            <div className="NFT-titre">VERIFIER UN ELEMENT</div>
+            {yeshash}
+        </div>
+    )
+    }
 }
-
-
-export default NftCheck
