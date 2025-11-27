2cc3fe3c97907ad0c9b6ed157cf81521bbfa0c19
pMeridjenManoukian <22168225+pMeridjenManoukian@users.noreply.github.com>
2025-11-27 10:58:24 +0100

    fix: remove output export for dynamic routes

--- tracevault/next.config.ts
+++ tracevault/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // ✅ Pour Vercel
+  reactStrictMode: true,
 };
 
 export default nextConfig;
